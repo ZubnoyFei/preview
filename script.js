ed84a907e233b6147391dfbf7c2cb4fb097a0474
Your Name <ustinovoleksij@gmail.com>
2022-04-25 13:45:35 +0300

    Added callback function

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
 'use strict';
 
 const box = document.querySelector('.box');
+
+
+/*Создадиим объект, который следит за элементом mutationRecords. Если он меняется, то выполняется эта функция*/
+
+let observer = new MutationObserver(mutationRecords => {
+    console.log(mutationRecords);
+});
+
+observer.observe(box,{ // В аргументах 2 объекта: объект, за которым мы следим; конфигурации этого объекта, за которыми нужно следить
+    childList: true
+});
+
+observer.disconnect();
